d1558392d3f198465cf741c62c15f58a77a9e0ae
Jeremy GROSJEAN <jeremy.grosjean.icam@gmail.com>
2021-11-27 16:50:06 +0100

    DEV : controller de Form, Question, Answer et Employee

---
+++ src/main/java/com/ecn/iemjava/controller/AnswerController.java
@@ -0,0 +1,42 @@
+package com.ecn.iemjava.controller;
+
+import com.ecn.iemjava.models.Answer;
+import com.ecn.iemjava.repository.AnswerRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/answer")
+public class AnswerController {
+
+    // Injection of Repository
+    AnswerRepository answerRepository;
+    public AnswerController(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    // Request to add an answer
+    // TODO: change type of return wether it is needed or not (Answer or void)
+    @PostMapping
+    public Answer addAnswer(@RequestBody Answer answer){
+        answerRepository.save(answer);
+        return answer;
+    }
+
+    // Request to get all the questions of the db
+    // TODO : personalize to target the user's specific answers (include user in path)
+    @GetMapping("/all")
+    public List<Answer> getAllAnswers(){
+        return answerRepository.findAll();
+    }
+
+    // Request to get specific question with its id
+    // TODO: deal with an Exception instead of returning "null" if the answer hasn't been found
+    @GetMapping("/{id}")
+    public Answer getAnswerById(@PathVariable("id") Integer id){
+        Optional<Answer> optionalAnswer = answerRepository.findById(id);
+        return optionalAnswer.orElse(null);
+    }
+}
--- src/main/java/com/ecn/iemjava/controller/EmployeeController.java
+++ src/main/java/com/ecn/iemjava/controller/EmployeeController.java
@@ -3,23 +3,42 @@ package com.ecn.iemjava.controller;
 import com.ecn.iemjava.models.Employee;
 import com.ecn.iemjava.models.User;
 import com.ecn.iemjava.repository.EmployeeRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    private EmployeeRepository employeeRepository;
 
+    // Injection of Repository
+    private EmployeeRepository employeeRepository;
     public EmployeeController(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
+    // Request to add an answer
+    // TODO: change type of return wether it is needed or not (Employee or void)
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee){
+        employeeRepository.save(employee);
+        return employee;
+    }
+
+    // Request to get all the questions of the db
+    // TODO : personalize to target the specific employee (include intermission? in path)
     @GetMapping("/all")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
+
+    // Request to get specific question with its id
+    // TODO: deal with an Exception instead of returning "null" if the employee hasn't been found
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable("id") Integer id){
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        return optionalEmployee.orElse(null);
+    }
+
 }
