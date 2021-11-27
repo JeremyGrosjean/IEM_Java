eddc1e38e5580b9ef5c0d10b172158cec006f713
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-11-27 22:30:35 +0100

    activities filters

--- src/main/java/com/ecn/iemjava/controller/ActivityController.java
+++ src/main/java/com/ecn/iemjava/controller/ActivityController.java
@@ -2,12 +2,13 @@ package com.ecn.iemjava.controller;
 
 import com.ecn.iemjava.models.Activity;
 import com.ecn.iemjava.repository.ActivityRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -23,4 +24,17 @@ public class ActivityController {
     public List<Activity> getAllActivities() {
         return activityRepository.findAll();
     }
+
+    @ResponseBody
+    @GetMapping("/week/{dateBeginning}/{dateEnding}")
+    public List<Activity> getCustomedActivities(@PathVariable ("dateBeginning") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateBeginning,
+                                           @PathVariable ("dateEnding") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateEnding) {
+        return activityRepository.findActivityByCurrentWeek(dateBeginning, dateEnding);
+    }
+
+//    @GetMapping("/{id}")
+//    public Article getArticleById(@PathVariable("id") Integer idArticle){
+//        Optional<Article> optional = articleRepository.fin  dById(idArticle);
+//        return optional.orElse(null);
+//    }
 }
--- src/main/java/com/ecn/iemjava/repository/ActivityRepository.java
+++ src/main/java/com/ecn/iemjava/repository/ActivityRepository.java
@@ -2,8 +2,14 @@ package com.ecn.iemjava.repository;
 
 import com.ecn.iemjava.models.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Integer> {
+    @Query("select a from Activity a where (a.date > ?1) and (a.date < ?2)")
+    List<Activity> findActivityByCurrentWeek(Date dateBeginning, Date dateEnding);
 }
