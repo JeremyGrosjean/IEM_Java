2862243665fd2de22dfa51830ea0a6f90ae914ec
Jeremy GROSJEAN <jeremy.grosjean.icam@gmail.com>
2021-11-29 18:29:04 +0100

    DEV: Refonte de la BDD et dvp controller FormQuestion, Intermission, Employee

--- src/main/java/com/ecn/iemjava/controller/IntermissionController.java
+++ src/main/java/com/ecn/iemjava/controller/IntermissionController.java
@@ -1,9 +1,30 @@
 package com.ecn.iemjava.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecn.iemjava.models.Intermission;
+import com.ecn.iemjava.repository.IntermissionRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/intermission")
 public class IntermissionController {
+
+    private IntermissionRepository intermissionRepository;
+
+    public IntermissionController(IntermissionRepository intermissionRepository) {
+        this.intermissionRepository = intermissionRepository;
+    }
+
+
+    @PostMapping
+    public Intermission addIntermission(@RequestBody Intermission intermission) {
+        intermissionRepository.save(intermission);
+        return intermission;
+    }
+
+    @GetMapping("/all")
+    public List<Intermission> getAllIntermission(){
+        return intermissionRepository.findAll();
+    }
 }
