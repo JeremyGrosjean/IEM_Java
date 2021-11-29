88486fc24c7a1824ba512dbed3bd1a8e5dbd9405
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-11-29 14:11:02 +0100

    add activity ok

--- src/main/java/com/ecn/iemjava/controller/ActivityController.java
+++ src/main/java/com/ecn/iemjava/controller/ActivityController.java
@@ -5,6 +5,7 @@ import com.ecn.iemjava.repository.ActivityRepository;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.*;
 import java.util.function.BiConsumer;
 
@@ -30,6 +31,12 @@ public class ActivityController {
         return sortActivities(activityRepository.findActivityByCurrentWeek(dateBeginning, dateEnding));
     }
 
+    @PostMapping
+    public Activity createActivity(@RequestBody Activity activity, HttpSession session){
+        activityRepository.save(activity);
+        return activity;
+    }
+
     public Map<Date, List<Activity>> sortActivities(List <Activity> activities){
         Map<Date, List<Activity>> sortedActivities = new HashMap<>();
         List<Activity> activitiesByDay = new ArrayList<>();
@@ -45,10 +52,4 @@ public class ActivityController {
         });
         return sortedActivities;
     }
-
-//    @GetMapping("/{id}")
-//    public Article getArticleById(@PathVariable("id") Integer idArticle){
-//        Optional<Article> optional = articleRepository.fin  dById(idArticle);
-//        return optional.orElse(null);
-//    }
 }
