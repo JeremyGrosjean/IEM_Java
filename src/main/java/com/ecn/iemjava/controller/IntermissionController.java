bbcb9534fb6d8000bfc5ef78e842f81d3623c7be
Jeremy GROSJEAN <jeremy.grosjean.icam@gmail.com>
2021-11-30 15:01:32 +0100

    DEV: implementation methode startDate intermission

--- src/main/java/com/ecn/iemjava/controller/IntermissionController.java
+++ src/main/java/com/ecn/iemjava/controller/IntermissionController.java
@@ -1,19 +1,24 @@
 package com.ecn.iemjava.controller;
 
+import com.ecn.iemjava.models.Employee;
 import com.ecn.iemjava.models.Intermission;
 import com.ecn.iemjava.repository.IntermissionRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/intermission")
 public class IntermissionController {
 
     private IntermissionRepository intermissionRepository;
+    private EmployeeController employeeController;
 
-    public IntermissionController(IntermissionRepository intermissionRepository) {
+    public IntermissionController(IntermissionRepository intermissionRepository, EmployeeController employeeController) {
         this.intermissionRepository = intermissionRepository;
+        this.employeeController = employeeController;
     }
 
 
@@ -26,5 +31,20 @@ public class IntermissionController {
     @GetMapping("/all")
     public List<Intermission> getAllIntermission(){
         return intermissionRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Intermission getIntermissionById(@PathVariable("id") Integer id){
+        Optional<Intermission> optionalIntermission = intermissionRepository.findById(id);
+        return optionalIntermission.orElse(null);
+    }
+
+    @PutMapping("/startdate/{id}/{startDate}")
+    public LocalDate setStartDateByEmployeeId(@PathVariable("id") String id, @PathVariable("startDate") LocalDate startDate){
+        Employee employee = employeeController.getEmployeeById(id);
+        Intermission intermission = intermissionRepository.getIntermissionByEmployee(employee);
+        intermission.setStartDate(startDate);
+        intermissionRepository.save(intermission);
+        return intermission.getStartDate();
     }
 }
