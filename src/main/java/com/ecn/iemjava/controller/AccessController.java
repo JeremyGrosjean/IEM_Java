28482693a1c98a3527fc4e5b7d24891cfb8567dc
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-12-01 09:39:07 +0100

    possibility to get a connected user

--- src/main/java/com/ecn/iemjava/controller/AccessController.java
+++ src/main/java/com/ecn/iemjava/controller/AccessController.java
@@ -1,9 +1,50 @@
 package com.ecn.iemjava.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecn.iemjava.models.Access;
+import com.ecn.iemjava.models.Employee;
+import com.ecn.iemjava.models.User;
+import com.ecn.iemjava.repository.AccessRepository;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/access")
-public class AccessController {
+public class AccessController extends HttpServlet {
+    AccessRepository accessRepository;
+
+    public AccessController(AccessRepository accessRepository) {
+        this.accessRepository = accessRepository;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Access access, HttpServletRequest request) {
+        try {
+            Access loginInfos = this.accessRepository.findByAccount(access.getAccount());
+            if (loginInfos.getPassword().equals(access.getPassword())) {
+                request.getSession().setAttribute("user", loginInfos.getUser());
+                request.getSession().setAttribute("LoginMessage", "LoginOk");
+            }
+            else {
+                request.getSession().setAttribute("LoginMessage", "Mot de passe erroné");
+            }
+        } catch (Exception e){
+            request.getSession().setAttribute("LoginMessage", "Cet utilisateur n'existe pas");
+        }
+        return (String)request.getSession().getAttribute("LoginMessage");
+    }
+
+    @GetMapping("current-user")
+    public User getCurrentUser(HttpServletRequest request){
+        return (User)request.getSession().getAttribute("user");
+    }
+
+    @GetMapping("/all")
+    public List<Access> getAllAccess(){
+        return this.accessRepository.findAll();
+    }
 }
--- src/main/java/com/ecn/iemjava/repository/AccessRepository.java
+++ src/main/java/com/ecn/iemjava/repository/AccessRepository.java
@@ -1,9 +1,17 @@
 package com.ecn.iemjava.repository;
 
 import com.ecn.iemjava.models.Access;
+import com.ecn.iemjava.models.Employee;
+import com.ecn.iemjava.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccessRepository extends JpaRepository<Access, Integer> {
+
+    Access findByAccount(String account);
+
+    @Query("SELECT a.user FROM Access a WHERE a.account =?1")
+    User findUserByAccount(String account);
 }
