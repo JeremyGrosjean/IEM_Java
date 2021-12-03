f4e423ca8be0abb976dbbdc4edc47d1c20522662
“Ali-Soilihi <ali-soilihi.chouaimbou@hotmail.fr>
2021-12-03 08:55:17 +0100

    dev: galére avec access pb avec user qui est abstract

--- src/main/java/com/ecn/iemjava/controller/AccessController.java
+++ src/main/java/com/ecn/iemjava/controller/AccessController.java
@@ -1,7 +1,11 @@
 package com.ecn.iemjava.controller;
 
 import com.ecn.iemjava.models.Access;
+import com.ecn.iemjava.models.Employee;
 import com.ecn.iemjava.repository.AccessRepository;
+import com.ecn.iemjava.repository.EmployeeRepository;
+import com.ecn.iemjava.repository.FormRepository;
+import com.ecn.iemjava.repository.IntermissionStatusRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,10 +14,17 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/access")
 public class AccessController {
-    public AccessController(AccessRepository accessRepository) {
+
+    // Injection of Repository
+    private EmployeeRepository employeeRepository;
+    private AccessRepository accessRepository;
+
+
+    public AccessController(AccessRepository accessRepository,EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
         this.accessRepository = accessRepository;
     }
-    private AccessRepository accessRepository;
+
 
     @GetMapping
     public List<Access> getAll(){
@@ -29,10 +40,18 @@ public class AccessController {
         return access;
     }
 
-    @PostMapping("/postaccess")
-    public Access postAccess(@RequestBody Access access){
-
-        return accessRepository.save(access);
+    @PostMapping("/register/{idUser}")
+    public Access postAccess(@RequestBody Access access, @PathVariable("idUser") String idUser){
+//        System.out.println(access.getUser().getId());
+//        Optional<Employee> optionalEmployee = employeeRepository.findById(access.getUser().getId());
+//        System.out.println(optionalEmployee.toString());
+////        if(access.getUser().getId()==optionalEmployee.stream().filter()){
+////
+////        }
+//        accessRepository.save(access);
+        AccesidUser  access1 = new Access();
+        access1 = access;
+        return access1;
     }
 
 
