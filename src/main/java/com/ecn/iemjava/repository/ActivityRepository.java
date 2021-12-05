1bdbf8245f40b6405bbcf8a871e0056864d576da
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-12-05 20:03:25 +0100

    filter dates works

--- src/main/java/com/ecn/iemjava/repository/ActivityRepository.java
+++ src/main/java/com/ecn/iemjava/repository/ActivityRepository.java
@@ -1,6 +1,7 @@
 package com.ecn.iemjava.repository;
 
 import com.ecn.iemjava.models.Activity;
+import com.ecn.iemjava.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,14 +9,17 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, String> {
-    @Query("select a from Activity a where (a.date >= ?1) and (a.date <= ?2)")
-    List<Activity> findActivityByCurrentWeek(LocalDate dateBeginning, LocalDate dateEnding);
+    @Query("select a from Activity a where (a.date >= ?1) and (a.date <= ?2) and (a.employee = ?3)")
+    List<Activity> findActivityByCurrentWeekByUser(LocalDate dateBeginning, LocalDate dateEnding, User user);
 
     @Query("select a from Activity a where a.date = :date")
     List<Activity> findActivityByOneDay(LocalDate date);
+
+    List<Activity> findAllByEmployee(User user);
 
 //    List<Activity> findAllByUser(String id);
 }
